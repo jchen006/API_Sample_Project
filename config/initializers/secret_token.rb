879c987b11b50d5acf10ee505097c7deb13c8522
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-12-23 21:10:51 -0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apisample::Application.config.secret_token = '12cc400cfa2ec6c017b390f8063287277760a23a6e903cd871cb2faecc48526b0772338fd5d3fb678583f5f005889f43ac61c58c61ee42c0c97ebb501c8a02f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Apisample::Application.config.session_store :cookie_store, key: '_apisample_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Apisample::Application.config.session_store :active_record_store
